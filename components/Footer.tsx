c5a70d6c01d4a173a6954d274851daf310c23921
hassan1400938 <mohammadwaqarulhassan@gmail.com>
2024-08-11 12:30:02 +0500

    fixed a bug and add robots.txt

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 import { socialMedia } from "@/data";
 import MagicButton from "./ui/MagicButton";
+import Image from "next/image";
 
 const Footer = () => {
   return (
@@ -39,14 +40,14 @@ const Footer = () => {
 
         <div className="flex items-center my-5 md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <a href={info.link}>
-              <div
-                key={info.id}
-                className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-              >
-                <img src={info.img} alt="icons" width={20} height={20} />
-              </div>
-            </a>
+            <div
+              key={info.id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <a href={info.link}>
+                <Image src={info.img} alt="icons" width={20} height={20} />
+              </a>
+            </div>
           ))}
         </div>
       </div>
